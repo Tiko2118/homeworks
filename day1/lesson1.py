6dde9dd201d53d32d3f1b8889fcf4c2d835b65af
Tiko2118 <tinatiniomanadze@gmail.com>
2022-09-29 07:00:58 -0700

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,27 @@
+name = "tiko"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "tiko" არის ცვლადისთვის მნიშვნელობა
+surname = "omanadze"
+
+
+ #print(name)
+
+
+name = "tiko" #ეს არის str (string) ტიპის ცვლადი
+age = 27 # ეს არის int (integer) მთელი რიცხვი
+height = 164 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი  
+
+knows_finans = True #True ან False
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+love_nature = True 
+have_doughter = True
+
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+print(name + " " + surname) 
+
+  
+
+print(name + " " + surname + " " + str(age) + " " + str(height) +" " +str(knows_finans) + " " + str(is_ugly) + " "+ str(love_nature) + " " + str(have_doughter))
