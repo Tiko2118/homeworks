1dbbf51421dc55cfc05d64fde5962ba8ce0c07b9
Tiko2118 <tinatiniomanadze@gmail.com>
2022-09-29 07:05:09 -0700

    second commit

---
+++ day2/lesson2.py
@@ -0,0 +1,59 @@
+name = "tiko"
+
+print (len(name))  #len length
+            #4
+
+my_sentence = """გამარჯობა, შეგახსენებთ, რომ სწავლას იწყებთ 19 ში"""
+
+
+ #print(my_sentence)
+
+# knows_ programming = True
+
+# if 10<5:
+   # print("hello")
+
+#tab-ის საშუალებით მოვახდინეთ ინდენტაცია (indentation)
+
+
+#full_name = "nika keshelava"
+#print(len(full_name))
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+#print(full_name[7])
+#position = index სინონიმებია
+
+#print("z" in full_name)
+#print("k" not in full_name)
+#print("n" in full_name)
+
+#full_name = "nika keshelava"
+#print(full_name[2:9])
+#print(full_name[2:9:2])
+#start: finish: step
+
+#print(full_name[:5])
+#print(full_name[5:])
+
+#full_name = "tiko omanadze"
+#print(full_name[:5])
+#print(full_name[5:])
+#print(full_name[-1])
+#print(full_name[-1:-6:-1])
+
+
+#სტრინგებს აქვთ მეთოდები
+# 1) სტრინგ.upper()
+#2)len(სტრინგ)
+
+full_name = "nika keshelava"
+print(full_name.upper())
+name2 = "  nika  "
+# print(name2.lower())
+#print(name2.strip())
+
+
+print(name2.replace(" ", "#"))
+
+name2 = " tiko  "
+print(name2.replace("","&"))
